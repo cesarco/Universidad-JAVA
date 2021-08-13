2a1ab397fc2ea3d15fb493974c80ad2ed1965c3b
rodri <rodri@DESKTOP-TASJ1IK>
2021-08-13 09:47:44 -0500

    creacion de Métodos en java ' / Aritemtica

---
+++ Java/Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones;
+
+/**
+ *
+ * @author rodri
+ */
+public class PruebaAritmetica {
+
+    public static void main(String[] args) {
+        Aritmetica aritmetica1 = new Aritmetica();
+        aritmetica1.a = 8;
+        aritmetica1.b = 2;
+        aritmetica1.sumar();
+    }
+}

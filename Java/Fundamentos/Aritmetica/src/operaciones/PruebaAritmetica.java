3fc65dfa4b5a3b161538e008c9a5c01d7f0cb624
rodri <rodri@DESKTOP-TASJ1IK>
2021-08-20 15:21:16 -0500

    Alcance de Variables en Java

--- Java/Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
+++ Java/Fundamentos/Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -12,6 +12,12 @@ package operaciones;
 public class PruebaAritmetica {
 
     public static void main(String[] args) {
+
+        // Variables locales
+        var a = 10;
+        var b = 2;
+        miMetodo();
+
         Aritmetica aritmetica1 = new Aritmetica();
         System.out.println("aritmetica1 a= " + aritmetica1.a);
         System.out.println("aritmetica1 b= " + aritmetica1.b);
@@ -20,5 +26,12 @@ public class PruebaAritmetica {
         System.out.println("aritmetica2 a= " + aritmetica2.a);
         System.out.println("aritmetica2 b= " + aritmetica2.b);
 
+    }
+
+    public static void miMetodo() {
+        // la variable a esta fuera del alcance donde fue definido
+        // a = 10;
+
+        System.out.println("Otro metodo");
     }
 }

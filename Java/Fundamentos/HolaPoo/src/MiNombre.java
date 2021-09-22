864beb45bedd1c2d837b1d2ac33dd94436648cc5
rodri <rodri@DESKTOP-TASJ1IK>
2021-09-22 09:53:49 -0500

    Commits antiguos sin subir a github

---
+++ Java/Fundamentos/HolaPoo/src/MiNombre.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rodri
+ */
+public class MiNombre {
+
+    public static void main(String args[]) {
+//        System.out.println("Hello word");
+//        System.out.println("Cesar Rodriguez");
+//        int edad = 19;
+//        String nombre = "Cesar Rodriguez";
+//        System.out.println("la edad es " + nombre + "es de: " + edad);
+//        edad = 25;
+//        nombre = "Lina Rodriguez";
+//        System.out.println("La edad de " + nombre + " es de " + edad);
+//        edad = 32;
+//        nombre = "Ruber Rodriguez";
+//        System.out.println("La edad de " + nombre + " es de " + edad);
+//        edad = 50;
+//        nombre = "Luis Rodriguez";
+//        System.out.println("La edad de " + nombre + " es de " + edad);
+
+//        int edad = -15;
+//
+//        if (edad >= 18) {
+//            System.out.println("Es mayor de edad");
+//        } else {
+//            System.out.println("Es menor de edad");
+//        }
+//          int numero1 = 5;
+//          int numero2 = 15;
+//          
+//          if(numero1 > numero2){
+//              System.out.println("numero1 es mayor");
+//          }else{
+//              System.out.println("El numero2 es mayor");
+//          }
+//        int velocidadPermiCiudad = 60;
+//        var velocidadDect = 65;
+//        int velocidadPernuCarretera = 80;
+//        var velocidadCarretera = 81;
+//
+//        if (velocidadDect > velocidadPermiCiudad) {
+//            System.out.println("excedio la velocidad permitida en ciudad Multa");
+//
+//        }
+//        if (velocidadCarretera > velocidadPernuCarretera) {
+//            System.out.println("excedio la velocidad permitida en carretera Multa");
+//        }
+//         else {
+//            System.out.println("Ninguna");
+//        }
+//        
+//          int velocidad = 50;
+//          if(velocidad > 80){
+//              System.out.println("Velocidad excedida en carretera");
+//          }else if(velocidad > 60){
+//              System.out.println("Velocidad excedida en ciudad");
+//          }else{
+//              System.out.println("Ninguna");
+//          }
+//    }
+//            float base = 4;
+//            float altura = (float)4.3;
+//            
+//            float area;
+//            float perimetro;
+//            
+//            area = base * altura;
+//            perimetro = 2*base+2*altura;
+//            
+//            System.out.println("EL área del rectangulo es " + area);
+//            System.out.println("El perimetro del rectangulo es " + perimetro);
+            
+    
+//        int numero1 = 12;
+//        int numero2 = 5;
+//         
+//                 
+//        int hipotenusa = numero1^2 + numero2^2;
+//        
+//        System.out.println(hipotenusa);
+//
+//          int x = 1;
+//          int y = -2;
+//          
+//          if(x > 0 && y > 0){
+//              System.out.println("Cuadrante 1");
+//              
+//          }else if(x < 0 && y > 0){
+//              System.out.println("Cuadrante 2");
+//          }else if(x < 0 && y < 0){
+//              System.out.println("Cuadrante 3");
+//          }else{
+//              System.out.println("Cuadrante 4");
+//          }
+//        
+           int dia = 20;
+           int mes = 12;
+           int año = 2001;
+           
+           int edad = (2021 - año);
+           String diaHoy = "20";
+           String mesHoy = "8";
+
+           
+           
+           System.out.println(edad + " años " + mesHoy + " meses y " + diaHoy + " dias");
+;
+    }
+            
+
+}

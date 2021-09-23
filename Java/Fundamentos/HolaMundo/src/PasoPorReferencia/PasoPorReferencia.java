c365700dd389b7705ae1f031cad476b2df7fa545
rodri <rodri@DESKTOP-TASJ1IK>
2021-09-22 20:29:30 -0500

    Cambio por referencia

--- Java/Fundamentos/HolaMundo/src/PasoPorReferencia/PasoPorReferencia.java
+++ Java/Fundamentos/HolaMundo/src/PasoPorReferencia/PasoPorReferencia.java
@@ -1,4 +1,3 @@
-
 package PasoPorReferencia;
 
 import clases.Persona;
@@ -8,16 +7,23 @@ import clases.Persona;
  * @author rodri
  */
 public class PasoPorReferencia {
+
     public static void main(String[] args) {
         Persona persona1 = new Persona();
-        
+
         persona1.nombre = "Cesar";
         System.out.println("persona1 nombre = " + persona1.nombre);
-        cambiaValor(persona1);
+        persona1 = cambiaValor(persona1);
         System.out.println("persona1 cambio nombre = " + persona1.nombre);
     }
-    
-    public static void cambiaValor(Persona persona){
+
+    public static Persona cambiaValor(Persona persona) {
+        
+        if (persona == null) {
+            System.out.println("valor de persona invalido : null");
+            return null;
+        }
         persona.nombre = "KARLA";
+        return persona;
     }
 }
